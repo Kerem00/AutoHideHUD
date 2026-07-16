1f321324744f6d78a2696c7637669eea9558db19
Kerem00 <kerembicen94@gmail.com>
2026-07-16 17:21:37 +0300

    fix namespace for future compatibility

--- src/DynamicHudManager.cs
+++ src/DynamicHudManager.cs
@@ -1,30 +1,33 @@
 ﻿using GlobalEnums;
 using UnityEngine;
 
-public class DynamicHudManager : MonoBehaviour
+namespace AutoHideHUD
 {
-    public float enemyDetectRadius = 15f;
-    private int enemyLayerMask = 1 << 11;
-    private float checkTimer = 0f;
-
-    void Update()
+    public class DynamicHudManager : MonoBehaviour
     {
-        if (UIManager.instance != null && UIManager.instance.uiState != UIState.PLAYING) return;
+        public float enemyDetectRadius = 15f;
+        private int enemyLayerMask = 1 << 11;
+        private float checkTimer = 0f;
 
-        checkTimer += Time.deltaTime;
-        if (checkTimer < 0.25f) return;
-        checkTimer = 0f;
+        void Update()
+        {
+            if (UIManager.instance != null && UIManager.instance.uiState != UIState.PLAYING) return;
 
-        HudGlobalHide.IsHidden = !CheckForEnemies();
-    }
+            checkTimer += Time.deltaTime;
+            if (checkTimer < 0.25f) return;
+            checkTimer = 0f;
 
-    bool CheckForEnemies()
-    {
-        if (HeroController.instance == null) return false;
+            HudGlobalHide.IsHidden = !CheckForEnemies();
+        }
 
-        Vector2 playerPos = HeroController.instance.transform.position;
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(playerPos, enemyDetectRadius, enemyLayerMask);
+        bool CheckForEnemies()
+        {
+            if (HeroController.instance == null) return false;
 
-        return colliders.Length > 0;
+            Vector2 playerPos = HeroController.instance.transform.position;
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(playerPos, enemyDetectRadius, enemyLayerMask);
+
+            return colliders.Length > 0;
+        }
     }
 }
